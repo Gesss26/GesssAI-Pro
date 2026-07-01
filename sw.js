35f74f54c5f43002a31f0e2a903ebb695cff57a6
Gesss26 <gesualdo.sangiorgi@gmail.com>
2026-07-01 12:54:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,48 @@
-const CACHE_NAME = 'gessai-cache-v1';
+const CACHE_NAME = 'statsbet-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Installa il Service Worker e cached i file locali
+// Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-// Intercetta le richieste di rete
+// Fetch
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  
-  // IGNORA le richieste a domini esterni (Firebase, API, ecc.)
-  // Lascia che vadano direttamente alla rete senza caching
-  if (url.origin !== self.location.origin) {
-    return; 
-  }
-
-  // Per le richieste locali, usa la strategia Cache First
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
